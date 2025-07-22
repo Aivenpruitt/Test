aab7b14d43ae19a0e2c92d16509b08d9167d89cf
Aivenpruitt <160081914+Aivenpruitt@users.noreply.github.com>
2025-07-22 01:26:37 -0700

    Update new.js

--- new.js
+++ new.js
@@ -1,1 +1,13 @@
+const SteamUser = require("steam-user");
 
+const client = new SteamUser();
+
+client.logOn({
+  accountName: process.env.STEAM_USERNAME,
+  password: process.env.STEAM_PASSWORD,
+});
+
+client.on("loggedOn", () => {
+  console.log("✅ Logged into Steam!");
+  client.setPersona(SteamUser.EPersonaState.Online);
+});
